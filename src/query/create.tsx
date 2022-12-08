4482055c86479a136a2e268a7a3cca81149d493b
nate-wilkins <nate-wilkins@code-null.com>
2022-12-08 15:20:17 -0500

    Update 'query/create' with correct suspense call

--- src/query/create.tsx
+++ src/query/create.tsx
@@ -396,7 +396,7 @@ export const createHook = <
                 getCacheStore,
                 onDone: finishProfiling,
                 args,
-              });
+              })();
             },
           },
         ) as Data,
